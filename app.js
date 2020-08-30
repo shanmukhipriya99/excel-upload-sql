cfda492a64afca091b77531489d38bb475bc05ab
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-30 10:50:46 +0530

    imported routes

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 //Importing routes
+const upload = require('./routes/insert');
+const display = require('./routes/display');
 
 //Importing middleware
 app.use(cors());
 app.use(express.json());
+app.use(upload);
+app.use(display);
 
 //listening...
 app.listen(port, () => {
