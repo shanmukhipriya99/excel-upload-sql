e7734cbb100522f5ded412b0d1031ff5cbec7a78
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-30 10:51:21 +0530

    inserting the excel sheet

--- routes/insert.js
+++ routes/insert.js
@@ -1,4 +1,3 @@
-const fs = require('fs'); 
 const express = require('express');
 const connection = require('../db/db');
 const multer = require('multer');
@@ -6,7 +5,7 @@ const readXlsxFile = require('read-excel-file/node');
 const router = new express.Router();
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-       cb(null, '/uploads/')
+       cb(null, './uploads') 
     },
     filename: (req, file, cb) => {
        cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname)
@@ -16,5 +15,20 @@ const storage = multer.diskStorage({
   const upload = multer({storage: storage});
 
 router.post('/upload', upload.single('file'), (req, res) => {
-    
+   //  console.log(req.file.path);
+    readXlsxFile(req.file.path).then((rows) => {
+       rows.shift();
+      //  console.log(rows);
+       let query = "INSERT INTO customer (sno, name, age) VALUES ?";
+       connection.query(query, [rows], (err, result) => {
+          if(err) {
+            console.log("err");
+             res.status(500).send(err);
+          } else {
+             res.status(201).send("Excel sheet uploaded");
+          }
+       });
+    });
 });
+
+module.exports = router;
